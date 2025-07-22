4f32ace888593f08a9544978af16e479d69b446b
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-22 09:14:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ app.use(errorHandler);
 sequelize.authenticate()
   .then(() => {
     console.log('PostgreSQL Connected');
-    return sequelize.sync();
+    return sequelize.sync({ alter: true });
   })
   .then(() => {
     const PORT = process.env.PORT || 5000;
