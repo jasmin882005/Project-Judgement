d7ee6f500ab26114ac1c5566555bc423a61b59af
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-22 10:08:19 +0530

    Update swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -2,30 +2,31 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Drone API',
-            version: '1.0.0',
-        },
-        servers: [
-            {
-                url: 'https://project-judgement.onrender.com',  
-                description: 'Render Deployed Server',
-            },
-        ],
-        components: {
-            securitySchemes: {
-                JWTAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                },
-            },
-        },
-        security: [{ JWTAuth: [] }],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Drone API',
+      version: '1.0.0',
+      description: 'API documentation for the Drone Management system (Judgement Project)',
     },
-    apis: ['./routes/*.js'],
+    servers: [
+      {
+        url: 'https://project-judgement.onrender.com',  // Render base URL
+        description: 'Render Deployed Server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        JWTAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ JWTAuth: [] }], // Applies JWTAuth globally
+  },
+  apis: ['./routes/*.js'], // Scan all route files
 };
 
 const specs = swaggerJsdoc(options);
