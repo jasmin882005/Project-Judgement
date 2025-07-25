9cd420e95543f8e9d9ba618a6a2372e24371de53
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-25 09:53:12 +0530

    Update Telemetry.js

--- backend/models/Telemetry.js
+++ backend/models/Telemetry.js
@@ -1,27 +1,33 @@
-// Import Sequelize components
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db'); // DB connection instance
+const sequelize = require('../config/db');
 
-// Define the Telemetry model (table: Telemetries)
 const Telemetry = sequelize.define('Telemetry', {
-  // ID of the drone sending the telemetry data
-  droneId: DataTypes.STRING,
-
-  // Current GPS location (latitude,longitude as string)
-  gps: {
-    type: DataTypes.JSON,  // true JSON type
+  droneId: {
+    type: DataTypes.STRING,
     allowNull: false
   },
-
-  // Drone's altitude in meters
-  altitude: DataTypes.FLOAT,
-
-  // Drone's speed in km/h (or any other unit used)
-  speed: DataTypes.FLOAT,
-
-  // Battery level in percentage (e.g., 78)
-  battery: DataTypes.INTEGER,
+  gps: {
+    type: DataTypes.JSON,
+    allowNull: false
+  },
+  altitude: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  speed: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  battery: {
+    type: DataTypes.INTEGER,
+    validate: {
+      min: 0,
+      max: 100
+    }
+  }
+}, {
+  tableName: 'telemetries',
+  timestamps: true
 });
 
-// Export the model for use in telemetryController and elsewhere
 module.exports = Telemetry;
