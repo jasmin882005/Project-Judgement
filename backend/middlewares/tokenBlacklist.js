6e5d2f8e1561565ef047c7ce5c3fb273d1d86e6a
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-25 09:29:39 +0530

    Update tokenBlacklist.js

--- backend/middlewares/tokenBlacklist.js
+++ backend/middlewares/tokenBlacklist.js
@@ -1,14 +1,30 @@
+// In-memory token blacklist (non-persistent)
 const tokenBlacklist = new Set();
 
-// Add token to blacklist and auto-remove after expiry
+/**
+ * Add token to blacklist and remove it after expiry.
+ * @param {string} token - JWT to blacklist
+ * @param {number} expiryMs - Token expiry duration in milliseconds
+ */
 function addToBlacklist(token, expiryMs = 3600000) {
   tokenBlacklist.add(token);
+  console.log(`[BLACKLIST] Added token. Will auto-remove in ${expiryMs / 1000}s`);
+
   setTimeout(() => {
     tokenBlacklist.delete(token);
+    console.log(`[BLACKLIST] Token auto-removed from blacklist.`);
   }, expiryMs);
+}
+
+/**
+ * Check if token is currently blacklisted
+ */
+function isBlacklisted(token) {
+  return tokenBlacklist.has(token);
 }
 
 module.exports = {
   tokenBlacklist,
-  addToBlacklist
+  addToBlacklist,
+  isBlacklisted
 };
