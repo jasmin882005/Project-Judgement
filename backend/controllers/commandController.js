7ccfe563f091874c713c24c7861b4da16ce1111b
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-25 22:02:29 +0530

    Update commandController.js

--- backend/controllers/commandController.js
+++ backend/controllers/commandController.js
@@ -29,7 +29,7 @@ exports.sendCommand = async (req, res) => {
 
     res.status(201).json(newCommand);
   } catch (err) {
-    // ❌ Log failure
+    // Log failure
     await logEvent({
       action: "COMMAND_FAILED",
       event: "Error sending command",
