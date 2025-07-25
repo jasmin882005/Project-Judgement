aab0c32445733d7c50a1608617ba037cddb4abe4
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-25 09:32:10 +0530

    Update validate.js

--- backend/middlewares/validate.js
+++ backend/middlewares/validate.js
@@ -1,16 +1,26 @@
 const { validationResult } = require('express-validator');
+const { logEvent } = require('../utils/logger');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      errors: errors.array().map(err => ({
-        field: err.param,
-        message: err.msg
-      }))
+    const formattedErrors = errors.array().map(err => ({
+      field: err.param,
+      message: err.msg
+    }));
+
+    // log validation failure
+    await logEvent({
+      action: 'VALIDATION_FAILED',
+      event: `Request validation failed on ${req.originalUrl}`,
+      createdBy: req.user?.email || 'anonymous',
+      type: 'warning',
+      source: 'validateRequest'
     });
+
+    return res.status(422).json({ errors: formattedErrors });
   }
 
-  next(); // No errors, go forward
+  next(); // No errors, continue
 };
